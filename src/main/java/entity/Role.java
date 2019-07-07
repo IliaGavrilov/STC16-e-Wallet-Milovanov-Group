971fa7dafd3d8d164d7424920f97535baff83adc
Girfanov Damir <girfanov.d.v@gmail.com>
2019-07-07 22:38:34 +0300

    POJO объекты Role и User

---
+++ src/main/java/entity/Role.java
@@ -0,0 +1,22 @@
+package main.java.entity;
+
+public class Role {
+    public int id;
+    public String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
