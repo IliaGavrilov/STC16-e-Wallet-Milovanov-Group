92e793b77098be970e62f919142304dd53dd51c1
Girfanov Damir <girfanov.d.v@gmail.com>
2019-07-16 17:09:56 +0300

    Тип банковского счёта и добавление в БД таблиц с банковским счётом и типом банковского счёта.

---
+++ src/main/java/com/entity/TypeOfBankBill.java
@@ -0,0 +1,42 @@
+package com.entity;
+
+
+import org.springframework.data.annotation.Id;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "typeofbankbill")
+public class TypeOfBankBill {
+    @Id
+    public int id;
+    @Column(name = "title")
+    public String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @OneToOne(optional = false, mappedBy = "typeOfBankBill")
+    private BankBill bankBill;
+
+    public BankBill getBankBill() {
+        return bankBill;
+    }
+
+    public void setBankBill(BankBill bankBill) {
+        this.bankBill = bankBill;
+    }
+}
+
