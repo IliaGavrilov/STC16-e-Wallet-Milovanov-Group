273a747e10677a59ba18dd3ada4ce3a757525a20
Girfanov Damir <girfanov.d.v@gmail.com>
2019-07-17 22:42:27 +0300

    Изменен id с int на long. Добавлена аннотация GeneratedValue. Добавлены поля: номер счёта(numberOfBill), процентная ставка(percentRate), срок действия(validity).

--- src/main/java/com/entity/BankBill.java
+++ src/main/java/com/entity/BankBill.java
@@ -7,21 +7,26 @@ import javax.persistence.*;
 @Table(name = "bankbill")
 public class BankBill {
     @Id
-    public int id;
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    public long id;
     @Column(name = "type")
     public String type;
     @Column(name = "name")
     public String name;
+    @Column(name = "numberOfBill", unique = true)
+    public int numberOfBill;
+    @Column (name = "percentRate")
+    public float percentRate;
+    @Column (name = "validity")
+    public long validity;
     @Column(name = "description")
     public String description;
-
-
 
     @OneToOne(optional = false)
     @JoinColumn(name="type_bank_bill_id", unique = true, nullable = false, updatable = false)
     private TypeOfBankBill typeOfBankBill;
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
@@ -59,6 +64,30 @@ public class BankBill {
 
     public void setTypeOfBankBill(TypeOfBankBill typeOfBankBill) {
         this.typeOfBankBill = typeOfBankBill;
+    }
+
+    public int getNumberOfBill() {
+        return numberOfBill;
+    }
+
+    public void setNumberOfBill(int numberOfBill) {
+        this.numberOfBill = numberOfBill;
+    }
+
+    public float getPercentRate() {
+        return percentRate;
+    }
+
+    public void setPercentRate(float percentRate) {
+        this.percentRate = percentRate;
+    }
+
+    public long getValidity() {
+        return validity;
+    }
+
+    public void setValidity(long validity) {
+        this.validity = validity;
     }
 
 }
