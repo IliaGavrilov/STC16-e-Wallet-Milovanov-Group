803fa06428e24c0b65f883823d283d3efd7f811f
Girfanov Damir <girfanov.d.v@gmail.com>
2019-08-02 01:26:01 +0300

    В импорты добавлен контроллер.

--- src/main/java/com/controllers/BankController.java
+++ src/main/java/com/controllers/BankController.java
@@ -2,9 +2,9 @@ package com.controllers;
 
 import com.entity.Bank;
 import com.repository.BankRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
