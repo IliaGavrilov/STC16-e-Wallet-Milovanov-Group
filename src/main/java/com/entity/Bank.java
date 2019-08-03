537390018b1e00d746d55cb4fa8d83339cad173d
Girfanov Damir <girfanov.d.v@gmail.com>
2019-08-04 01:41:21 +0300

    Внесены изменения для реализации связи между объектами

--- src/main/java/com/entity/Bank.java
+++ src/main/java/com/entity/Bank.java
@@ -4,63 +4,86 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity(name = "bank")
 public class Bank {
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private long id;
-    private String name;
-    private String description;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private String description;
 
 
-    @ManyToMany(cascade = {
-            CascadeType.PERSIST,
-            CascadeType.MERGE
-    })
-    @JoinTable(name = "bank_users",
-            joinColumns = @JoinColumn(name = "bank_id"),
-            inverseJoinColumns = @JoinColumn(name = "user_id")
-    )
-    @JsonManagedReference
-    private Set<User> users = new HashSet<>();
+	@ManyToMany(cascade = {
+			CascadeType.PERSIST,
+			CascadeType.MERGE
+	})
+	@JoinTable(name = "bank_users",
+			joinColumns = @JoinColumn(name = "bank_id"),
+			inverseJoinColumns = @JoinColumn(name = "user_id")
+	)
+	@JsonManagedReference
+	private Set<User> users = new HashSet<>();
 
-    public Bank() {
-    }
 
-    public Bank(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
+	public void addUser(User user) {
+		users.add( user );
+		user.getBanks().add( this );
+	}
 
-    public void addUser(User user) {
-        users.add(user);
-        user.getBanks().add(this);
-    }
+	public void removeUser(User user) {
+		users.remove( user );
+		user.getBanks().remove( this );
+	}
 
-    public void removeUser(User user) {
-        users.remove(user);
-        user.getBanks().remove(this);
-    }
 
-    public long getId() {
-        return id;
-    }
+	@OneToMany(mappedBy = "bank",
+			cascade = CascadeType.MERGE,
+			orphanRemoval = true)
+	private List<BankBill> bankBills;
 
-    public String getName() {
-        return name;
-    }
+	public void addBankBill(BankBill bankBill) {
+		bankBills.add( bankBill );
+		bankBill.setBank( this );
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void removeBankBill(BankBill bankBill) {
+		bankBills.remove( bankBill );
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public List<BankBill> getBankBills() {
+		return bankBills;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setBankBills(List<BankBill> bankBills) {
+		this.bankBills = bankBills;
+	}
+
+	public Bank(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
