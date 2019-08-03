a892d21c6fa9f3bb0149f84ae0bafc6d4e8163b0
Girfanov Damir <girfanov.d.v@gmail.com>
2019-08-04 01:42:28 +0300

    Добавил List

--- src/main/java/com/repository/BankBillRepository.java
+++ src/main/java/com/repository/BankBillRepository.java
@@ -4,6 +4,14 @@ import com.entity.BankBill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 @Repository
 public interface BankBillRepository extends JpaRepository<BankBill, Long> {
+
+    List<BankBill> bankBills = new ArrayList<BankBill>();
+
 }
