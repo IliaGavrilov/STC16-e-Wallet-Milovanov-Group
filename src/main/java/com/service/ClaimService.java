116e12da55809422bff6b0ab2a3f7cfaef867df8
Girfanov Damir <girfanov.d.v@gmail.com>
2019-08-09 21:04:27 +0300

    1

--- src/main/java/com/service/ClaimService.java
+++ src/main/java/com/service/ClaimService.java
@@ -7,14 +7,17 @@ import com.entity.User;
 import java.util.List;
 
 public interface ClaimService {
-    void addClaim(User user, long productId);
-    void addClaim(long userId, long productId);
+
+    void addClaim(User user, BankBill productId);
+
+    void addClaim(long userId, BankBill productId);
 
     Claim getClaimById(long id);
 
     List<BankBill> getAllBills();
 
     List<Claim> getAllUserClaims(User user);
+
     List<Claim> getAllUserClaims(long userId);
 
     void removeClaimById(int id);
