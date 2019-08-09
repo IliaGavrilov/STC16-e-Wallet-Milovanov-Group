d577b2026e1bed0f24ecb53302a915591edc6f27
Girfanov Damir <girfanov.d.v@gmail.com>
2019-08-09 21:05:09 +0300

    4

---
+++ src/main/java/com/serviceImpl/ClaimConfirmServiceImpl.java
@@ -0,0 +1,26 @@
+package com.serviceImpl;
+
+import com.entity.Claim;
+import com.repository.ClaimRepository;
+import com.service.ClaimConfirmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service(value = "claimConfirmService")
+public class ClaimConfirmServiceImpl implements ClaimConfirmService {
+
+	@Autowired
+	@Qualifier("claimRepository")
+	private ClaimRepository claimRepository;
+
+	public void acceptClaim(Claim claim) {
+		claimRepository.findClaimById( claim.getId() ).setStatus( Claim.StatusEnum.Accept );
+	}
+
+	public void resetClaim(Claim claim) {
+		claimRepository.findClaimById( claim.getId() ).setStatus( Claim.StatusEnum.Negative );
+	}
+
+
+}
