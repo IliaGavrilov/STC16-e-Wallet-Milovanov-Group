b6bca65c23ef29865f999676a9bff82c859f0739
Aleksandr <ab114@yandex.ru>
2019-08-12 01:14:59 +0300

    Авторизация Изменены конфиги

--- src/main/java/com/AppConfig.java
+++ src/main/java/com/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
+//@EnableWebMvc
 @Configuration
 @ComponentScan({"com.controllers"})
 @Import({SecurityConfig.class})
@@ -17,20 +17,11 @@ public class AppConfig {
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
         driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
-        driverManagerDataSource.setUrl("jdbc:postgresql://ec2-54-247-170-5.eu-west-1.compute.amazonaws.com:5432/d1qsn4vbbh1t4");
-        driverManagerDataSource.setUsername("yavknxrhegcjyx");
-        driverManagerDataSource.setPassword("c38b2288b7f0d450f64eec0baaae8cbadd43fb4ccd754768fb0e6e002e291144");
+        driverManagerDataSource.setUrl("jdbc:postgresql://127.0.0.1/postgres");
+        driverManagerDataSource.setUsername("postgres");
+        driverManagerDataSource.setPassword("postgres");
         return driverManagerDataSource;
     }
 
-//    @Bean(name = "dataSource")
-//    public DriverManagerDataSource dataSource() {
-//        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-//        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
-//        driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
-//        driverManagerDataSource.setUsername("postgres");
-//        driverManagerDataSource.setPassword("landlord17");
-//        return driverManagerDataSource;
-//    }
 
 }
